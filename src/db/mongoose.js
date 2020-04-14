d3528a506a26486529b23f79789e56344c127686
ruturajkanake <ruturajkanake1999@gmail.com>
2020-04-14 14:11:56 +0530

    files removed

--- src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(()=>
-    console.log('MongoDB connected')
-).catch(err => console.log(err))
