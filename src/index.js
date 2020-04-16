d741b09862db0c27d96fe99840e3da7f71de4255
ruturajkanake <ruturajkanake1999@gmail.com>
2020-04-17 01:16:22 +0530

    production build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const app = require('./app')
 const port = process.env.PORT
+const path = require('path')
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+    })
+}
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
