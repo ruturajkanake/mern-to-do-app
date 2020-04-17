34b32f71306a9eb9c9314da928916522a2b98da2
ruturajkanake <ruturajkanake1999@gmail.com>
2020-04-17 15:56:07 +0530

    created relative path in app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,16 @@ app.use(express.json())
 
 // Dont need to include for development 
 
+const path = require('path')
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
+    })
+}
+
 app.use('/',userRouter)
 app.use('/',taskRouter)
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ const app = require('./app')
 const port = process.env.PORT || 5000
 
 // Don't need to include this for development
-const path = require('path')
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static( 'client/build' ));
-
-    app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
-    })
-}
 
 // end;
 
