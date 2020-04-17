89e958ac165052ff3d7527088c3d86b80c37f95b
ruturajkanake <ruturajkanake1999@gmail.com>
2020-04-17 19:12:02 +0530

    Removed unwanted comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 const app = require('./app')
 const port = process.env.PORT || 5000
 
-// Don't need to include this for development
-
-
-// end;
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
